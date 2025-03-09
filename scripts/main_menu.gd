02b6f61ff8a57b09b4e735bbf0dea2bb9a4cc9ca
joeyvongphasouk <joey.vongpha@gmail.com>
2025-03-09 01:24:33 -0700

    added main menu changes, options now works on both main menu and ingame, player physics changes, weapon clipping and accurate shadows shader is also added

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,11 +1,23 @@
 extends CanvasLayer
-@onready var start: Button = $VBoxContainer/Start
+@onready var start: Button = $MenuPanel/VBoxContainer/Start
+@onready var menu_panel: Control = $MenuPanel
+@onready var options_menu: CanvasLayer = $options_menu
 
 func _ready() -> void:
 	start.grab_focus()
+	options_menu.exit_options_menu.connect(on_exit_options_menu)
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/playground.tscn")
 
+func _on_options_pressed() -> void:
+	menu_panel.visible = false
+	options_menu.visible = true
+	options_menu.set_process(true)
+	
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func on_exit_options_menu() -> void:
+	menu_panel.visible = true
+	options_menu.visible = false
