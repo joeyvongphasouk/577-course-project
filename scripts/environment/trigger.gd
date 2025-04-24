8c0b761eab2c595c7be509c4e2ea135ea7299d1d
sovannsam <ssam@mines.edu>
2025-04-23 23:07:54 -0600

    level 3 added and demo removed

--- scripts/environment/trigger.gd
+++ scripts/environment/trigger.gd
@@ -6,12 +6,14 @@ signal trigger_change
 
 func _turn_on():
 	emit_signal("trigger_change")
+	print("on")
 	for obj in indicators:
 		if obj.has_method("_trigger_on"):
 			obj._trigger_on()
 	pass
 
 func _turn_off():
+	print("offf")
 	emit_signal("trigger_change")
 	for obj in indicators:
 		if obj.has_method("_trigger_off"):
